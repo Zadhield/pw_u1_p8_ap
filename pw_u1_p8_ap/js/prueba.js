68da7eddca90526cb7ec10d10c87faf3eae0a10a
Angelo Pujota <167159608+Zadhield@users.noreply.github.com>
2025-05-07 19:31:58 -0500

    Add files via upload

---
+++ pw_u1_p8_ap/js/prueba.js
@@ -0,0 +1,16 @@
+let num1=0;
+let num2=0;
+let resultado=0;
+function sumar(){
+ num1 = document.getElementById("num1").value;
+ num2 = document.getElementById("num2").value;
+ resultado= num1+num2;
+}
+function validar(resultado){
+    if(resultado %=0){
+        resultado = 0 ;
+    } else{
+        resultado = x ;
+    }
+}
+console.log(resultado);
